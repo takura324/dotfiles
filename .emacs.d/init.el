19740d67039376a5bce78b30e62d3837a19d5b8f
Takura Koyama <takurandom324@gmail.com>
2016-11-08 14:40:22 +0900

    bash.exe を Linux では commen out

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -535,13 +535,14 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 (require 'shell)
-(setq explicit-shell-file-name "bash.exe")
-(setq shell-command-switch "-c")
-(setq shell-file-name "bash.exe")
+(when (eq system-type 'cygwin)
+  (setq explicit-shell-file-name "bash.exe")
+  (setq shell-command-switch "-c")
+  (setq shell-file-name "bash.exe")
 
-;; (M-! and M-| and compile.el)
-(setq shell-file-name "bash.exe")
-(modify-coding-system-alist 'process ".*sh\\.exe" 'utf-8)
+  ;; (M-! and M-| and compile.el)
+  (setq shell-file-name "bash.exe")
+  (modify-coding-system-alist 'process ".*sh\\.exe" 'utf-8))
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
