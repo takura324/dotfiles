cfd78bdbd81dc7cbd821cee210f6f9d29023774f
Takura Koyama <takurandom324@gmail.com>
2016-12-26 09:13:59 +0900

    Git の設定を独立したファイル 21-git.el に移動。

---
+++ .emacs.d/inits/21-git.el
@@ -0,0 +1,21 @@
+(require 'bind-key)
+
+;;----------------------------------------------------------------------
+;; magit
+;;----------------------------------------------------------------------
+;;(package-install 'magit)
+
+;; Magit
+(setq-default magit-auto-revert-mode nil)
+(setq vc-handled-backends '())
+(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+(bind-key "C-x m" 'magit-status)
+(bind-key "C-c l" 'magit-blame)
+
+(custom-set-faces
+ '(magit-diff-added ((t (:background "black" :foreground "green"))))
+ '(magit-diff-added-highlight ((t (:background "white" :foreground "green"))))
+ '(magit-diff-removed ((t (:background "black" :foreground "blue"))))
+ '(magit-diff-removed-hightlight ((t (:background "white" :foreground "blue"))))
+ '(magit-hash ((t (:foreground "red"))))
+)
